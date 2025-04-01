6ac29b0f09865d7022d67bc1a99ef650394d603a
Andry2127 <73197499+Andry2127@users.noreply.github.com>
2025-04-01 18:03:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,77 @@
 from typing import Optional, List, Union
 
-from fastapi import FastAPI, Query, HTTPException, status
+from fastapi import FastAPI, HTTPException, status
 import uvicorn
 from pydantic import BaseModel
 
-from products import products_data
 
+from tasks import do_task
 
 app = FastAPI()
 
 
-class ProductModel(BaseModel):
+class TasksModel(BaseModel):
     id: int
     name: str
-    price: Optional[float] = None
-
-@app.get("/product/{product_id}/", response_model=ProductModel, status_code=status.HTTP_200_OK)
-async def get_product(product_id: int):
-    product = [product for product in products_data if product.get("id") == product_id]
-    if product:
-        return product[0]
+    difficult: Optional[float] = None 
     
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+@app.get("/tasks/{task_id}/", response_model=TasksModel,  status_code=status.HTTP_200_OK)
+async def get_task(task_id: int):
+    task = [task for task in do_task if task.get("id") == task_id]
+    if task:
+        return task[0]
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
 
 
-@app.get("/products/", response_model=List[ProductModel], status_code=status.HTTP_200_OK)
-async def get_products():
-    return products_data    
+@app.get("/tasks/", response_model=List[TasksModel], status_code=status.HTTP_200_OK)
+async def get_task():
+    return do_task
+
+@app.post("/tasks/", status_code=status.HTTP_201_CREATED)
+async def create_task(task: TasksModel):
+    do_task.append(task.model_dump())
+    return dict(msg="Стврено.")
 
 
+@app.put("/tasks/{task_id}/", response_model = TasksModel, status_code=status.HTTP_202_ACCEPTED)
+async def update_taskd(task_id: int, taskmod: TasksModel):
+    for task in do_task:
+        if task.get("id") == task_id:
+            task["id"] = taskmod.model_dump()["id"]
+            task["name"] = taskmod.model_dump()["name"]
+            task["difficult"] = taskmod.model_dump()["difficult"]
+            return task
 
-@app.post("/product/", status_code=status.HTTP_201_CREATED)
-async def create_product(product: ProductModel):
-    products_data.append(product.model_dump())
-    return dict(message="Product created successfully")
 
-
-@app.put("/product/{product_id}/", status_code=status.HTTP_200_OK)
-async def update_product(product_id: int, product_model: ProductModel):
-    for product in products_data:
-        if product.get("id") == product_id:
-            product["id"] = product_model.model_dump()["id"]
-            product["name"] = product_model.model_dump()["name"]
-            product["price"] = product_model.model_dump()["price"]
-            return product
-
-@app.patch("/product/{param}/{product_id}/", status_code=status.HTTP_202_ACCEPTED)
-async def update_param_product(param: str,product_id: int, value: Union[str, float, int]):
-    for product in products_data:
-        if product.get("id") == product_id:
-            if param in product:
-                product[param] = value
-                return 
-            
-
+@app.patch("/tasks/{param}/{tasks_id}/", status_code=status.HTTP_202_ACCEPTED)
+async def update_task(param: str, task_id: int, value: Union[str, int, float]):
+    for tasktaskuct in do_task:
+        if tasktaskuct.get("id") == task_id:
+            if param in tasktaskuct:
+                tasktaskuct[param] = value
+                return
             else:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
-           
+                raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Param invalid")
 
 
-
-@app.delete("/product/",status_code=status.HTTP_200_OK)
-async def delete_product(product:ProductModel):
-    products_data.remove(product.dict())
+@app.delete("/tasks/",status_code=status.HTTP_200_OK)
+async def delete_task(task:TasksModel):
+    do_task.remove(task.dict())
     return dict(msg = "Видалено" )
 
 
-@app.delete("/products/{product_id}/", response_model=ProductModel, status_code=status.HTTP_200_OK)
-async def delete_product(product_id: int):
-    product = next((product for product in products_data if product["id"] == product_id), None)
-    if product:
-        products_data.remove(product) 
-        return product
+@app.delete("/tasks/{task_id}/", response_model=TasksModel, status_code=status.HTTP_200_OK)
+async def delete_task(task_id: int):
+    task = next((task for task in do_task if task["id"] == task_id), None)
+    if task:
+        do_task.remove(task) 
+        return task
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-
-
-
 if __name__ == "__main__":
-    uvicorn.run("main:app")
-
+    uvicorn.run("main:app", port=8001)
 
 
 
